fd40500a1037660afdccd300aa1a910fd0d80b6e
Eduardo <eduardoantune12@gmail.com>
2025-06-19 15:06:42 -0300

    Commitando alterações feitas do módulo 31 até o 32

---
+++ src/containers/ListaDeTarefas/index.tsx
@@ -0,0 +1,39 @@
+import { useSelector } from 'react-redux'
+import Tarefa from '../../components/Tarefa'
+import { Container } from './styles'
+
+import { RootReducer } from '../../store'
+
+const ListaDeTarefas = () => {
+  const { itens } = useSelector((state: RootReducer) => state.tarefas)
+  const { termo } = useSelector((state: RootReducer) => state.filtro)
+
+  const filtraTarefas = () => {
+    return itens.filter(
+      (item) => item.titulo.toLowerCase().search(termo.toLowerCase()) >= 0
+    )
+  }
+
+  return (
+    <Container>
+      <p>
+        2 tarefas marcados como: &quot;categoria&ldquo; e &quot;{termo}&ldquo;
+      </p>
+      <ul>
+        {filtraTarefas().map((t) => (
+          <li key={t.titulo}>
+            <Tarefa
+              id={t.id}
+              descricao={t.descricao}
+              status={t.status}
+              titulo={t.titulo}
+              prioridade={t.prioridade}
+            />
+          </li>
+        ))}
+      </ul>
+    </Container>
+  )
+}
+
+export default ListaDeTarefas
