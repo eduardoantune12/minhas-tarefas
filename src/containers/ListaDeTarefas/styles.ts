2039d387727b8fcc4bbe22da354ead6ee728c52c
Eduardo <eduardoantune12@gmail.com>
2025-06-20 15:18:33 -0300

    Exibindo tratando e estilizando a mensagem de filtros selecionados

--- src/containers/ListaDeTarefas/styles.ts
+++ src/containers/ListaDeTarefas/styles.ts
@@ -5,3 +5,11 @@ export const Container = styled.main`
   height: 100vh;
   overflow-y: scroll;
 `
+
+export const Resultado = styled.p`
+  display: block;
+  font-weight: bold;
+  font-size: 18px;
+  color: #000000;
+  padding: 40px 0;
+`
